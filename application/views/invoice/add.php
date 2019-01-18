2b3e82c1ae790d50fc6d08a1b989b68a576c3f05
Men Nguyen <nvmen@tma.com.vn>
2019-01-18 19:07:20 +0700

    add code item value

--- application/views/invoice/add.php
+++ application/views/invoice/add.php
@@ -88,74 +88,213 @@
    <div class="card">
       <div class="card-header">
          <h4 class="card-title">Dịch vụ</h4>
-         <div class="search">               
-            <input type="text" name ="search_text" id ="search_text" class="form-control input-sm" maxlength="64" placeholder="Search" value="" />
-            <button type="submit" name="submit" class="btn btn-search btn-info btn-fill pull-right">Search</button>
+         <div class="row">
+            <div class="col-md-4">
+                 <label>Loại dịch vụ</label>
+                 <select class="form-control" id="service">
+                     <?php
+                      foreach($services as $row)
+                      {
+                       
+                        echo '<option value ="'.$row['id'].'" >'.$row['name'].'</option>';
+                       
+                      }
+                      ?>      
+                   
+                  </select>
+            </div>
+            <div class="col-md-2">
+                <div class="form-group">
+                    <label>Số lượng</label>
+                    <input type="text" name ="qty" id ="qty" class="form-control input-sm number"  style="width:100px" maxlength="64" placeholder="Số lượng" value="1" />
+                    <button type="button" id="add-service" name="add-service" style="margin-top: 20px" class="btn btn-search btn-info btn-fill pull-right" onclick="selected_service()">Thêm</button> 
+                 </div>
+            </div>
+                          
+            
          </div>
  
       </div>
       <div class="card-body">
+                 <div class="card-body table-full-width table-responsive">
+                     <table class="table table-hover table-striped">
+                        <thead>                           
+                            <th>Dịch vụ</th>
+                            <th>Số lượng</th>
+                            <th>Đơn giá</th>
+                            <th>Thành Tiền</th>
+                            <th class="text-center">Action</th>
+                        </thead>
+                        <tbody id ="content-table">
+                           
+                        </tbody>
+                    </table>
+                    <div class="col-md-12"> 
+                        <span>Tong cong</span>
+                    </div>
+                 </div>
+
+
       </div>
    </div>
 </div>
 
 <script type="text/javascript">
-    
-     $(document).ready(function () {
+
+var services_list = <?php echo json_encode($services) ?>;
+var services_selected = [];
+var total_money = 0;
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function tableCreate() {
+  
+    var open_tr ="<tr>";
+    var oclose_tr ="</tr>";
+    var res ="";
+    for(var i = 0; i<services_selected.length; i++){
+        var row ="<tr>"+
+                    "<td>"+services_selected[i].service.name+"</td>" +
+                    "<td>"+services_selected[i].qty+"</td>" +
+                    "<td>"+numberWithCommas(services_selected[i].service.price)+" đ</td>" +
+                    "<td>"+ numberWithCommas(services_selected[i].total) +" đ</td>" +
+                    "<td><a href='javascript:void(0)' onclick = 'show_delete("+services_selected[i].id+")' class='btn btn-danger btn-xs'>"+
+                         "<span class='glyphicon glyphicon-remove'></span>Del</a>"+
+                    "</td>" +
+                "</tr>";
+
+        res =res +row;        
+    }
+    $("#content-table").html(res);
+
+}
+
+
+function show_delete(id){
+  
+  debugger;
+    for( var i = 0; i < services_selected.length; i++){ 
+           if ( services_selected[i].id == id) {
+                services_selected.splice(i, 1); 
+                break;
+           }
+   }
+   tableCreate();
+}
+function selected_service(){
+
+    var qty = $("#qty").val();
+    var obj =  {};
+    var service_id = $("#service").val();
+   
+    for( var i =0;i< services_list.length;i++){
+
+        if(services_list[i].id == service_id){           
+            obj = services_list[i];
+            break;
+        }
+
+    }
+    var find = false;
+    for( var i =0;i< services_selected.length;i++){
+
+        if(services_selected[i].id == service_id){
+            find = true;
+            break;
+        }
+
+    }
+   
+    if(qty <= 0){
+        swal("Lỗi!", "Xin vui lòng chọn số lượng", "warning");
+        return;
+    }
+   
+    if(find){
+        swal("Lỗi!", "Dịch vụ này đã được thêm vào rồi", "warning");
+        return;
+    }else {
+        var objAdd = {
+            id:service_id,
+            service:obj,
+            qty:qty,
+            total: obj.price*qty
+        }
+        services_selected.push(objAdd);
+        tableCreate();
+
+    }
+
+
+   // alert(qty +" "+service_id)
+}
+$(document).ready(function () {
 
 
       var options = {
 
-           url: function(phrase) {
-             return "<?php echo base_url();?>customer/ajax";
-      },
+            url: function(phrase) {
+                 return "<?php echo base_url();?>customer/ajax";
+            },
 
-     getValue: function(element) {
-       return element.fullname;
-     },
-    template: {
-              type: "description",
-              fields: {
-                  description: "phone"
-              }
-          },
+             getValue: function(element) {
+               return element.fullname;
+             },
+            template: {
+                      type: "description",
+                      fields: {
+                          description: "phone"
+                      }
+                  },
 
-     ajaxSettings: {
-          dataType: "json",
-          method: "POST",
-          data: {
-            dataType: "json"
-          }
-     },
+             ajaxSettings: {
+                  dataType: "json",
+                  method: "POST",
+                  data: {
+                    dataType: "json"
+                  }
+             },
 
-  preparePostData: function(data) {
-    data.search = $("#search_text").val();
-    return data;
-  },
+            preparePostData: function(data) {
+                data.search = $("#search_text").val();
+                return data;
+            },
 
-  list: {
-     onClickEvent: function() {
-         var id = $("#search_text").getSelectedItemData().id;
-         $.post( "<?php echo base_url();?>customer/ajaxcust", { id: id })
-           .done(function( data ) {
-               console.log(data);
-              $("#fullname").val(data.fullname);
-              $("#phone").val(data.phone);
-              $("#email").val(data.email);
-              $("#address").val(data.address);
-           });
-                 
-      }
-   },
+            list: {
+                 onClickEvent: function() {
+                     var id = $("#search_text").getSelectedItemData().id;
+                     $.post( "<?php echo base_url();?>customer/ajaxcust", { id: id })
+                       .done(function( data ) {
+                           console.log(data);
+                          $("#fullname").val(data.fullname);
+                          $("#phone").val(data.phone);
+                          $("#email").val(data.email);
+                          $("#address").val(data.address);
+                       });
+                             
+                  }
+            },
 
-  requestDelay: 400
-};
-
-
+         requestDelay: 400
+    };
      
-$("#search_text").easyAutocomplete(options);
+    $("#search_text").easyAutocomplete(options);
+    jQuery('input.number').keyup(function(event) {
+
+      // skip for arrow keys
+      if(event.which >= 37 && event.which <= 40) return;
+
+      // format number
+      jQuery(this).val(function(index, value) {
+        return value
+        .replace(/\D/g, "")
+        .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+        ;
+      });
+    });
 
 
-     });
+});
     
 </script>
